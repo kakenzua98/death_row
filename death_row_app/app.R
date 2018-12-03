d96c426fae8629f13d87a04d4fabeed19ce2d92f
kakenzua98 <kakenzua@college.harvard.edu>
2018-12-03 11:43:41 -0500

    trying to render map for all states

--- death_row_app/app.R
+++ death_row_app/app.R
@@ -13,10 +13,12 @@ library(ggrepel)
 library(datasets)
 library(wordcloud2)
 
+#figPath = system.file("examples/peace.png", package = "wordcloud2")
 top_words <- read_rds("top_words.rds")
 sentiment_by_time <- read_rds("sentiment_by_time.rds")
 race_sentiment_tbl <- read_rds("race_sentiment_tbl.rds")
 word_cloud <- read_rds("word_cloud.rds")
+word_freq <- read_rds("word_freq.rds")
 
 race_choices <- c("All",
                   "Black",
@@ -28,13 +30,23 @@ ui <- fluidPage(
   h1("Sentiment Analysis of Death Row Executions"),
   tabsetPanel(
     tabPanel(
+      title = "Introduction",
+      h1("Map")),
+    tabPanel(
+      title = "Why Texas?",
+      h1("Map")),
+    tabPanel(
       title = "Most Common Words",
       h1("Column"),
       numericInput("number", "Number of Words", 15, 1),
       plotOutput("wordPlot"),
       h1("Word Cloud"),
-      numericInput(inputId = "num", label = "Maximum number of words",
+      numericInput(inputId = "num_cloud", label = "Maximum number of words",
                  value = 100, min = 5),
+      #CAUSES AN ERROR WHENEVER I: 
+  #     # sliderInput("slider1", label = h3("Slider"), min = 0, 
+  #     max = 100, value = 50)
+  # )
       #colourInput("col", "Background colour", value = "white"),
       wordcloud2Output("cloud")
       
@@ -46,19 +58,28 @@ ui <- fluidPage(
       selectInput(inputId = "race",
                   label = "Race:",
                   choices = race_choices,
-                  selected = "All"),
+                  selected = "Black"),
       sliderInput(inputId = "age", label = "Age",
                   min = 27, max = 67,
                   value = c(30, 50)),
       checkboxInput(inputId = "line", 
                     label = "Show Best Fit Line", 
                     value = FALSE),
+      checkboxInput(inputId = "race_sent_tbl", 
+                    label = "Show Summary Table", 
+                    value = FALSE),
+      #checkboxGroupButtons(inputId = "Id038"),
       plotOutput("timePlot"),
       DT::dataTableOutput("table")),
     
     tabPanel(
       title = "Age Plot",
-      plotOutput("agePlot"))
+      plotOutput("agePlot")),
+  
+    tabPanel(
+      title = "Analysis",
+      h1("Map"))
+    
     
     
     
@@ -87,7 +108,9 @@ server <- function(input, output) {
    
    output$cloud <- renderWordcloud2({
      # Create a word cloud object
-     create_wordcloud(word_cloud$word, num_words = 50, background = "white")
+
+     #wordcloud2(word_freq, figPath = "texas.png", size = 1.5,color = "skyblue")
+     wordcloud2(word_freq, size = 2, shape = 'circle')
    })
    
    output$agePlot <- renderPlot({
@@ -149,9 +172,13 @@ server <- function(input, output) {
      
    })
    
+
    output$table <- DT::renderDataTable({
-     race_sentiment_tbl 
+     if (input$race_sent_tbl == TRUE) {
+       race_sentiment_tbl 
+     }
    })
+
    
    
 }
